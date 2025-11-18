0b241e9898d2d2d2c9c718fb391cd973b77b3d48
Luis Forero <foluis@hotmail.com>
2025-11-18 13:48:28 -0600

    Change framework to .Net 8

--- CapgeminiMexicoCodingTest/Program.cs
+++ CapgeminiMexicoCodingTest/Program.cs
@@ -5,7 +5,9 @@
         static void Main(string[] args)
         {          
             IExercice exercice;
-                        
+
+            //Uncomment the exercice you want to run
+
             //exercice = new ShortestLengthFirst();
             //exercice = new AnagramChecker();
             exercice = new PrefixFinder();
